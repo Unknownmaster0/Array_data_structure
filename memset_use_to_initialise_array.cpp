0e33e999b55128c0d60bfbd46acd09cb1e730c84
Unknownmaster0 <sagar192002singh@gmail.com>
2023-09-07 16:51:18 +0530

    memset use in array

---
+++ memset_use_to_initialise_array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[8];
+    memset(arr,-1,sizeof(arr));
+    for(int i = 0; i<8; i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+    return 0;
+}
